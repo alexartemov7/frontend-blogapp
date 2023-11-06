f0eed3c877ae4da0cb47c2df4bc908c0b0c02ad6
Alex Artemov <artemov.sandro@gmail.com>
2023-11-06 16:44:35 -0500

    added readme file to the blog-app frontend

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,10 +9,10 @@ export default function Nav() {
 
       <header>
         <ul>
-          <li><a href="/" target="_blank" rel="noopener noreferrer">Home</a></li>
-          <li><a href="/new-post" target="_blank" rel="noopener noreferrer">Add Post</a></li>
-          <li><a href="/login" target="_blank" rel="noopener noreferrer">Login</a></li>
-          <li><a href="/signup" target="_blank" rel="noopener noreferrer">Sign up</a></li>
+          <li><a href="/" rel="noopener noreferrer">Home</a></li>
+          <li><a href="/new-post" rel="noopener noreferrer">Add Post</a></li>
+          <li><a href="/login" rel="noopener noreferrer">Login</a></li>
+          <li><a href="/signup" rel="noopener noreferrer">Sign up</a></li>
           <li style={{color: 'white'}}>{userState.email}</li>
 
         </ul>
